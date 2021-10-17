e4fd7bba3b413d35bcf59b2b70b8959564a46ad2
allen <theallenma@gmail.com>
2021-10-17 16:07:32 -0700

    Use arc to show css named colors

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import './App.css';
 const csvUrl =
   'https://gist.githubusercontent.com/alleniac/0bd95735c0e6ccc0cc84975f8027da99/raw/c6db8cd8c0600f5a8d0810f66a19ebb24e863264/cssNamedColors.csv';
 
+const width = window.innerWidth;
+const height = window.innerHeight;
+const centerX = width / 2;
+const centerY = height / 2;
+
+const pieArc = d3.arc().innerRadius(0).outerRadius(width);
+
 function App() {
   const [data, setData] = useState(null);
 
@@ -14,7 +21,27 @@ function App() {
     });
   }, []);
 
-  return <div></div>;
+  if (!data) {
+    return <pre>Loading...</pre>;
+  }
+
+  console.log(data);
+
+  return (
+    <svg width={width} height={height}>
+      <g transform={`translate(${centerX}, ${centerY})`}>
+        {data.map((d, index) => (
+          <path
+            d={pieArc({
+              startAngle: (index / data.length) * 2 * Math.PI,
+              endAngle: ((index + 1) / data.length) * 2 * Math.PI,
+            })}
+            fill={d['RGB hex value']}
+          ></path>
+        ))}
+      </g>
+    </svg>
+  );
 }
 
 export default App;
