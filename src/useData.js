9ca276f15e90c9e1d7aae5fd04395d8be363168e
allen <theallenma@gmail.com>
2021-10-30 20:49:28 -0700

    useData

---
+++ src/useData.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+import { csv } from 'd3';
+
+const csvUrl = '';
+
+export const useData = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const row = (d) => {
+      return d;
+    };
+    csv(csvUrl, row).then((data) => {
+      setData(data);
+    });
+  }, []);
+};
