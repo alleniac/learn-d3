c31411800f71d9b05fab4d53c1a8961f8e5f86ec
allen <theallenma@gmail.com>
2021-10-30 20:42:24 -0700

    Remove unnecessary things

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
 import './App.css';
-import { range } from 'd3-array';
-import { Face } from './Face';
-
-const width = 160;
-const height = 160;
-
-const array = range(20);
 
 function App() {
-  return (
-    <>
-      {array.map(() => (
-        <Face
-          width={width}
-          height={height}
-          centerX={width / 2}
-          centerY={height / 2}
-          strokeWidth={10}
-          eyeOffsetX={30}
-          eyeOffsetY={30}
-          eyeRadius={10}
-          mouthWidth={10}
-          mouthRadius={40}
-        />
-      ))}
-    </>
-  );
+  return <></>;
 }
 
 export default App;
